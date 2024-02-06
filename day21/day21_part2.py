b1385b8a935fe2fc61f23f16511bc92d522fdba5
Marcel <marcel.cata@univrses.com>
2024-02-06 22:58:44 +0100

    Day 21: Part 2

--- day21/day21_part2.py
+++ day21/day21_part2.py
@@ -1,47 +1,87 @@
 # (c) Copyright 2023 Marcel
-import copy
+from collections import deque
 
 
-def get_neighbours(point, garden):
-    row, col = point
-    neighbours = []
-    if row != 0:
-        neighbours.append((row - 1, col))
-    if row != (len(garden) - 1):
-        neighbours.append((row + 1, col))
+def compute_reachable_plots(garden, start_row, start_col, steps):
+    size = len(garden)
+    reachable_plots = set()
+    visited_plots = set((start_row, start_col))
+    queue = deque()
+    queue.append((start_row, start_col, steps))
+    while queue:
+        row, col, steps = queue.popleft()
+        if steps % 2 == 0:
+            reachable_plots.add((row, col))
+        if steps == 0:
+            continue
+        
+        # Check the neighbours
+        for next_row, next_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+            if next_row < 0 or next_row >= size or next_col < 0 or next_col >= size:
+                continue
+            if garden[next_row][next_col] == "#":
+                continue
+            if (next_row, next_col) in visited_plots:
+                continue
+            visited_plots.add((next_row, next_col))
+            queue.append((next_row, next_col, steps - 1))
 
-    if col != 0:
-        neighbours.append((row , col - 1))
-    if col != (len(garden[0]) - 1):
-        neighbours.append((row , col + 1))
-
-    return [neighbour for neighbour in neighbours if garden[neighbour[0]][neighbour[1]] != "#"]
+    return len(reachable_plots)
 
 
 def main():
     with open("day21/input.txt") as f:
         garden = f.readlines()
-    
-    num_steps = 26501365
+    num_steps = 64
     start = "S"
-    start_point = None
+    start_row, start_col = None, None
     for i, row in enumerate(garden):
         for j, _ in enumerate(row):
             if garden[i][j] == start:
-                start_point = (i, j)
-    positions = [start_point]
-
-    for i in range(num_steps):
-        print(i)
-        new_positions = []
-        for point in positions:
-            neighbours = get_neighbours(point, garden)
-            for neighbour in neighbours:
-                if neighbour not in new_positions:
-                    new_positions.append(neighbour)
-        positions = copy.deepcopy(new_positions)
+                start_row, start_col = i, j
     
-    print(f"Part 1 result: {len(positions)}")
+    print(f"Part 1 result: {compute_reachable_plots(garden, start_row, start_col, num_steps)}")
+
+    num_steps = 26501365
+    size = len(garden)  # Garden is squared
+    
+    num_gardens = num_steps // size - 1  # Amount of full gardens that can be travelled in same direction
+
+    odd = (num_gardens // 2 * 2 + 1) ** 2
+    even = ((num_gardens + 1) // 2 * 2) ** 2
+
+    # Compute value for fully reachable plots
+    odd_points = compute_reachable_plots(garden, start_row, start_col, size * 2 + 1)
+    even_points = compute_reachable_plots(garden, start_row, start_col, size * 2)
+
+    # Compute value for corner plots
+    corner_t = compute_reachable_plots(garden, size - 1, start_col, size - 1)
+    corner_r = compute_reachable_plots(garden, start_row, 0, size - 1)
+    corner_b = compute_reachable_plots(garden, 0, start_col, size - 1)
+    corner_l = compute_reachable_plots(garden, start_row, size - 1, size - 1)
+
+    # Compute value for plots that are reached only on one of their corners, specified by tblr
+    small_tr = compute_reachable_plots(garden, size - 1, 0, size // 2 - 1)
+    small_tl = compute_reachable_plots(garden, size - 1, size - 1, size // 2 - 1)
+    small_br = compute_reachable_plots(garden, 0, 0, size // 2 - 1)
+    small_bl = compute_reachable_plots(garden, 0, size - 1, size // 2 - 1)
+
+    # Compute value for plots that are fully reached except on one of their corners, specified by tblr
+    large_tr = compute_reachable_plots(garden, size - 1, 0, size * 3 // 2 - 1)
+    large_tl = compute_reachable_plots(garden, size - 1, size - 1, size * 3 // 2 - 1)
+    large_br = compute_reachable_plots(garden, 0, 0, size * 3 // 2 - 1)
+    large_bl = compute_reachable_plots(garden, 0, size - 1, size * 3 // 2 - 1)
+
+    num_reachable_plots = (
+        odd * odd_points + even * even_points + corner_t + corner_r + corner_b + corner_l +
+        (num_gardens + 1) * (small_tr + small_tl + small_br + small_bl) +
+        num_gardens * (large_tr + large_tl + large_br + large_bl)
+    )
+
+    print(f"Part 2 result: {num_reachable_plots}")
+
+
+
             
 
 
